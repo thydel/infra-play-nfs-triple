c4c0c5e790238780d3d07fed9ea1376804198155
Thierry Delamare <t.delamare@laposte.net>
2020-06-12 18:46:20 +0200

    Adds inventory conf

--- conf.mk
+++ conf.mk
@@ -7,7 +7,7 @@ $~: dir.root := $(abspath $(or $(LOC_ROOT), $(loc_root), /usr/local))
 $~: dir.set := $(or $(LOC_SET), $(loc_set), epi)
 $~: - := $(foreach _, etc bin lib, $(eval $~: dir.$_ := $(dir.root)/$_))
 $~: dir.base := $(dir.etc)/$(dir.set)
-$~: - := $(foreach _, src data doc repo, $(eval $~: dir.$_ := $(dir.base)/$_))
+$~: - := $(foreach _, inventory src data doc repo, $(eval $~: dir.$_ := $(dir.base)/$_))
 $~: - := $(if $(filter $(dir.base), $(wildcard $(dir.base))),, $(error dubious basedir $(dir.base)))
 
 # Local Variables:
