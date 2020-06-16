5a8b801a95b5261fa53d4df5a3f187466740b7db
Thierry Delamare <t.delamare@laposte.net>
2020-06-16 11:03:38 +0200

    Adds readme

--- repo.mk
+++ repo.mk
@@ -7,6 +7,15 @@ $~: url != git config --get remote.origin.url
 $~: branch :=
 $~: branch != git branch --show-current
 $~: name := $(basename $(notdir $(url)))
+$~: readme := $$($~.README)
+
+define $~.README
+# Installed via
+
+export GIT_SSH_COMMAND='$(shell git config core.sshCommand)';
+git clone $(name) -b $(branch);
+make -C $(name) install;
+endef
 
 # Local Variables:
 # indent-tabs-mode: nil
