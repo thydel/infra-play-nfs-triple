a42fa0458cafcc4ceb15fe692336297c521ba6d3
Thierry Delamare <t.delamare@laposte.net>
2020-06-16 13:22:21 +0200

    Adds install and help

--- Makefile
+++ Makefile
@@ -4,23 +4,26 @@ include head.mk
 include conf.mk
 include lineinfile.mk
 
+helps :=
+
 links := repo inventory
 $(foreach link,$(links),$(call Import,conf,dir.$(link)))
 $(links):; ln -s $(conf.dir.$@) $@
 links: phony $(links)
-links.help := Links $(links) to base out dir
-help += links
+links.help := Links "$(links)" to base out dir
+helps += links
 
 exclude := *~ tmp out $(links)
 exclude.git := .git/info/exclude
 $(foreach _,$(exclude),$(call lineinfile, $_, $(exclude.git)))
 exclude: phony .git/info/exclude
-exclude.help := Adds $(exclude) to $(exclude.git)
-help += exclude
+exclude.help := Adds "$(exclude)" to "$(exclude.git)"
+helps += exclude
 
 once := links exclude
 once: phony $(once)
-help.once := Runs once targets $(once)
+once.help := Runs once targets ($(once))
+helps += once
 
 ifdef NEVER
 migrate = git -C $(src) format-patch --stdout --root $@ | git am -p1
@@ -36,7 +39,17 @@ $(files):; $(migrate)
 migrate:: phony $(files)
 endif
 
-main: phony; nfs-triple.mk --no-print-directory
+main install: phony; nfs-triple.mk --no-print-directory --warn-undefined-variables $@
+main.help := nfs-triple.mk main
+install.help := nfs-triple.mk install
+helps += main install
+
+help: self := $(strip $(if $(filter Makefile, $(TOP)), make, $(TOP)))
+help: help := echo;
+help: help += $(foreach _, $(helps), echo '$(self) $_: $($_.help)';)
+help: help += echo;
+help: help += nfs-triple.mk --no-print-directory help;
+help:; @($($@))
 
 # Local Variables:
 # indent-tabs-mode: nil
--- nfs-triple.mk
+++ nfs-triple.mk
@@ -1,14 +1,70 @@
 #!/usr/bin/make -f
 
 include head.mk
+include repo.mk
+include conf.mk
+
+tmp := tmp
+out := out
+install :=
+helps :=
 
 . := repo/infra-data-nfs-triple
 
-tmp/nfs-triple.json: nfs-triple.jsonnet; $< -J $. > $@
+~ := $(tmp)/nfs-triple.mk
+$~: jq = .triples | "$* := " + join(" ")
+$~: $(tmp)/%.mk : $./%.json; jq -r '$(jq)' $< > $@
+$(TOP): $~
+-include $~
+mk: phony $~
+mk.help := Generates "$~" makefile setting the list of triple to be include
+helps += mk
 
-out/playbook.%.json: tmp/nfs-triple.json out/.stone; jq 'map(select(.tags|index("$*")))' $< > $(@)
+~ := $(tmp)/nfs-triple.json
+$~: $(tmp)/%.json : %.jsonnet $./%.json; $< -J $. > $@
+plays: phony $~
+plays.help := Generates all playbook in "$~"
+helps += plays
 
-tags: jq := .triples|map("$(TOP) --no-print-directory out/playbook." + . + ".json")[]
-tags:; @jq -r '$(jq)' $./nfs-triple.json | dash
+~ := $(out)/playbook.%.json
+$~: jq = map(select(.tags|index("$*")))
+$~: $(tmp)/nfs-triple.json $(out)/.stone; jq '$(jq)' $< > $(@)
+install += $($($(TOP)):%=$~)
+tags: phony $(install)
+tags.help := Generates one playbook per triple in "$(out)/playbook.%.json"
+helps += tags
+
+ifdef NEVER
+tags: jq := .triples|map("$(TOP) --no-print-directory $(out)/playbook." + . + ".json")[]
+tags: phony; @jq -r '$(jq)' $./nfs-triple.json | dash
+endif
 
 main: phony tags
+main.help := $(TOP) tags
+helps += main
+
+$(call Import, conf, dir.repo)
+$(call Import, repo, name branch readme)
+
+~ := $(out)/README
+$~: repo.mk; echo "$(repo.readme)" > $@
+install += $~
+readme: phony $~
+readme.help := Generates README for install dir
+helps += readme
+
+~ := install
+$~.link := $(conf.dir.repo)/$(repo.name)
+$~.dir := $($~.link).$(repo.branch)
+$~.files := $(install:$(out)/%=$($~.dir)/%)
+$($~.files): $($~.dir)/% : $(out)/%; install -CD -m=0444 --backup=numbered $< $@
+$($~.link): $($~.dir)/.stone; ln -snf $(<D) $@
+$~: phony $($~.link) $($~.files)
+install.help := \n\tinstall "$(out)/playbook.%.json" in "$($~.dir)"
+install.help += \n\tsymlink $($~.link) to $($~.dir)
+helps += install
+
+help: help := echo;
+help: help += $(foreach _, $(helps), echo -e '$(TOP) $_: $($_.help)';)
+help: help += echo;
+help:; @($($@))
