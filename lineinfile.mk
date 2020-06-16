dd537785d914431ab287cdf9bd425c605d02fe72
Thierry Delamare <t.delamare@laposte.net>
2020-06-16 15:03:19 +0200

    Allows --warn-undefined-variables

--- lineinfile.mk
+++ lineinfile.mk
@@ -8,7 +8,9 @@ $(adam):; touch -d @0 $@
 
 old-or-young := && echo $(adam) || echo $(ultimo)
 
-need-adam  = $(if $($(strip $1-need-adam)),, $(eval $(strip $1-need-adam := _)) $(eval $(strip $1:: $(adam))))
+need-adam   = $(eval $1-need-adam :=)
+need-adam  += $(if $($(strip $1-need-adam)),, $(eval $(strip $1-need-adam := _))
+need-adam  += $(eval $(strip $1:: $(adam))))
 
 lineinfile = $(eval $(strip $2:: $(shell grep -q '^$(strip $1)$$' $2 $(old-or-young)); echo '$(strip $1)' >> $$@))
 lineinfile += $(call need-adam, $2)
